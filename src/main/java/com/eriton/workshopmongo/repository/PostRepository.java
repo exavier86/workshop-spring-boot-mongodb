d80bbd5b6f08aa774102be0adcac35cb2e56a33e
Eriton Xavier <ertao1@hotmail.com>
2022-02-18 07:43:39 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/eriton/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.eriton.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eriton.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
